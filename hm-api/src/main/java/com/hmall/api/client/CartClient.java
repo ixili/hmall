c1bccfce38ea442348424855c30650ac52e3d1cd
ixili <lixi5201@163.com>
2023-10-26 07:19:37 +0800

    拆分user、trade、pay服务成功

---
+++ hm-api/src/main/java/com/hmall/api/client/CartClient.java
@@ -0,0 +1,13 @@
+package com.hmall.api.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+
+@FeignClient("cart-service")
+public interface CartClient {
+    @DeleteMapping("/carts")
+    void deleteCartItemByIds(@RequestParam("ids") Collection<Long> ids);
+}
--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -1,8 +1,11 @@
 package com.hmall.api.client;
 
 import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
@@ -16,4 +19,7 @@ import java.util.List;
 public interface ItemClient {
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @PutMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
 }
